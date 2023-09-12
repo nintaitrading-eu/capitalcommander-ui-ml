c8bb31b4e1cbc8c221378012b0287914fabf7c28
Andy Nagels <thereisanewway@gmail.com>
2023-09-12 21:27:57 +0200

    Implementation of asset usage + fix dune file.

--- capitalcommander/bin/error.eml.ml
+++ capitalcommander/bin/error.eml.ml
@@ -5,11 +5,24 @@ let custom_error_template _error debug_info suggested_response =
 
   Dream.set_header suggested_response "Content-Type" Dream.text_html;
   Dream.set_body suggested_response begin
-    <html>
-      <body>
-        <h1><%i code %> <%s reason %></h1>
-        <pre><%s debug_info %></pre>
-      </body>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="assets/css/pure-min.css" />
+        <link rel="stylesheet" href="assets/css/fontawesome/css/fontawesome.min.css" />
+        <link rel="stylesheet" href="assets/css/fontawesome/css/solid.min.css" />
+        <link rel="stylesheet" href="assets/css/custom_style.css" />
+        <link rel="icon" type="image/png" href="assets/favicon.png" />
+        <title>Error</title>
+        </head>
+        <body>
+        <div id="main">
+            <h1><%i code %> <%s reason %></h1>
+            <pre><%s debug_info %></pre>
+        </div>
+        </body>
     </html>
+    <html>
   end;
   Lwt.return suggested_response
--- capitalcommander/bin/main.ml
+++ capitalcommander/bin/main.ml
@@ -1,7 +1,14 @@
+let loader _root path _request =
+  match Assets.read path with
+  | None -> Dream.empty `Not_Found
+  | Some asset -> Dream.respond asset
+
 let () =
   Dream.run ~error_handler:(Dream.error_template Error.custom_error_template)
   @@ Dream.logger
   @@ Dream.router [
+
+    Dream.get "/assets/**" @@ Dream.static ~loader "";
 
     Dream.get "/status" (fun _ ->
         Dream.html (Printf.sprintf "Running..."));
