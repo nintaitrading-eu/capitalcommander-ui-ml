79062c7cfd8a2b5ef3081b112768360d07baae40
Andy Nagels <thereisanewway@gmail.com>
2023-09-12 13:53:21 +0200

    First implementation of template rendering via files, working for the /version route.

--- capitalcommander/bin/main.ml
+++ capitalcommander/bin/main.ml
@@ -1,29 +1,18 @@
-let successful = ref 0
-let failed = ref 0
-
-let count_requests inner_handler request =
-  try%lwt
-    let%lwt response = inner_handler request in
-    successful := !successful + 1;
-    Lwt.return response
-
-  with exn ->
-    failed := !failed + 1;
-    raise exn
-
 let () =
   Dream.run
   @@ Dream.logger
-  @@ count_requests
   @@ Dream.router [
 
-    Dream.get "/fail"
-      (fun _ ->
-        raise (Failure "The Web app failed!"));
+    Dream.get "/status" (fun _ ->
+        Dream.html (Printf.sprintf "Running..."));
+
+    Dream.get "/version" (fun _ ->
+      Dream.html (Version.render ()));
+
+    Dream.get "/accounts" (fun _ ->
+        Dream.html (Printf.sprintf "Running..."));
 
     Dream.get "/" (fun _ ->
-      Dream.html (Printf.sprintf
-        "%3i request(s) successful<br>%3i request(s) failed"
-        !successful !failed));
+      Dream.html (Printf.sprintf "Welcome."));
 
   ]
